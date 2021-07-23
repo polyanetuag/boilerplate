35f4af5227e5d1c56f369db10080af43df3c6787
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-23 20:18:42 -0300

    tipando uma propriedade

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({ title }: Props) {
   return (
     <div className={styles.container}>
       <Head>
