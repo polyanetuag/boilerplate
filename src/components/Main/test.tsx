4ba7573055c10a3eec39abb53fbd30ec5b33c7c8
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-24 19:39:42 -0300

    Atualizando arquivo de testes

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,10 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the color correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ background: '#06092b' })
+  })
 })
