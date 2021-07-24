ebbbe44f33cea4fa8e845eed620aa0a8cf529af5
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-24 20:15:21 -0300

    Instalando Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/preset-typescript",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
