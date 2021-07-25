d28f7c919d2b887f0bb55f0988dcf686006ce16b
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-24 21:26:52 -0300

    Config storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,6 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/components/**/stories.tsx"],
+  addons: [
     "@storybook/preset-typescript",
     "@storybook/addon-links",
     "@storybook/addon-essentials"
