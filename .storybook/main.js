74811dfe154fb4729831bd109047bd8e7dec9ec6
Polyane Tuag <pollyportugues@hotmail.com>
2022-01-04 19:53:40 -0300

    adding addon-knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,11 @@
 module.exports = {
-  stories: ["../src/components/**/stories.tsx"],
+  stories: ['../src/components/**/stories.tsx'],
   addons: [
-    "@storybook/preset-typescript",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    '@storybook/preset-typescript',
+    '@storybook/addon-links',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs'
+
+
   ]
 }
