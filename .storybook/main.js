e7bec143ed8876e245b5c3c18bc5d51726815473
Polyane Tuag <pollyportugues@hotmail.com>
2022-01-05 19:36:00 -0300

    typescript removal

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: [
-    '@storybook/preset-typescript',
     '@storybook/addon-links',
     '@storybook/addon-actions',
     '@storybook/addon-knobs'
