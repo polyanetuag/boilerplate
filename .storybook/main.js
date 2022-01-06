caac75076544daebe8279e5579f042d8958552c2
Polyane Tuag <pollyportugues@hotmail.com>
2022-01-06 20:36:23 -0300

    importing essential addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,6 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs'
-
-
+    "@storybook/addon-essentials",
   ]
 }
